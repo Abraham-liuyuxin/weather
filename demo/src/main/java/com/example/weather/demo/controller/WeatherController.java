603cb1ea09de0af1e69c65824c420d4021529986
zns <liu1131451057@gmail.com>
2020-11-14 00:54:08 +0800

    Revert "Revert "add comments""
    This reverts commit 298cf73e

--- demo/src/main/java/com/example/weather/demo/controller/WeatherController.java
+++ demo/src/main/java/com/example/weather/demo/controller/WeatherController.java
@@ -2,7 +2,7 @@ package com.example.weather.demo.controller;
 
 import com.example.weather.demo.model.Test;
 import com.example.weather.demo.service.WeatherService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,6 +16,9 @@ public class WeatherController {
     @Resource
     private WeatherService weatherService;
 
+    /**
+     * @return 返回数据库中的城市名列表
+     */
     @RequestMapping("/getCityname")
     public List<String> getCityname(){
         List<Map<String,String>> list = weatherService.getCityname();
@@ -26,20 +29,29 @@ public class WeatherController {
         return citylist;
     }
 
+    /**
+     * @param js for example:{"cityname":"北京"}
+     */
     @RequestMapping("/getbyname")
     public List<Map<String,Object>> getbyname(@RequestParam Map<String,String> js){
         List<Map<String,Object>> list = weatherService.getbyname(js);
         return list;
     }
 
+    /**
+     * @param js for example:{"cityname":"北京", "year":"2018"}
+     */
     @RequestMapping("/getbyn_y")
     public List<Map<String,Object>> getbyn_y(@RequestParam Map<String,String> js){
         List<Map<String,Object>> list = weatherService.getbyname(js);
         return list;
     }
 
+    /**
+     * @param js for example:{"cityname":"北京", "year":"2018", "month":"08"}
+     */
     @RequestMapping("/getbyn_y_m")
-    public List<Map<String,Object>> getbyn_y_m(@RequestParam String js){
+    public List<Map<String,Object>> getbyn_y_m(@RequestParam Map<String,String> js){
 //        System.out.println(cityname);
 //        System.out.println(year);
 //        System.out.println(month);
@@ -47,13 +59,16 @@ public class WeatherController {
 //        js.put("cityname","北京");
 //        js.put("year","2018");
 //        js.put("month","02");
-
-        //List<Map<String,Object>> list = weatherService.getbyname(js);
+        List<Map<String,Object>> list = weatherService.getbyname(js);
         System.out.println(js);
-        List<Map<String,Object>> list = null;
         return list;
     }
 
+    /**
+     * 若用axios库的post传递参数，请用Qs库的stringify()将对象化序列的参数转化为url形式
+     * @param js for example:{"cityname":"北京", "year":"2018", "month":"08", "day":"08"}
+     * @return 返回一个列表，该列表里只有该天的天气数据
+     */
     @RequestMapping("/getbyn_y_m_d")
     public List<Map<String,Object>> getbyn_y_m_d(@RequestParam Map<String,String> js){
 
@@ -62,12 +77,12 @@ public class WeatherController {
     }
 
     @RequestMapping("/test_conn2")
-    public Map<String,String> response2(@RequestParam(required = false) Map<String,Object> test){
+    public Map<String,String> response2(@RequestParam(required = false) Map<String,String> test){
         String id = "error2";
         String name = "行不通2";
         try {
-            id = test.get("id").toString();
-            name = test.get("name").toString();
+            id = test.get("id");
+            name = test.get("name");
         } catch (Exception e){
             System.out.println(e);
         }
